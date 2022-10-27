84098c728a072fa5bb3819095a8349d16e3aeec6
nrcool <nomanalam7214@gmail.com>
2022-10-27 10:37:33 +0200

    faker package, CRUD operation on mongodb through seedJS

--- Week-3-Databases/seeding-mongoDB/seed.js
+++ Week-3-Databases/seeding-mongoDB/seed.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const UsersCollection = require("./models/usersSchema.js")
 //Mongoose ODM object data modeling Library
+const { faker } = require("@faker-js/faker")
 
 // connect to Mongodb
 //mongoose.connect(URI, options, callback)
@@ -13,34 +14,67 @@ mongoose.connect("mongodb://127.0.0.1:27017/seeding-data"/*  ,()=>{
 /* use seeding-data */
 
 // seed data into users collection
-async function addUser(){
+function addUser(){
     const user = new UsersCollection({
-        firstName:"Naqvi",
-        lastName:"Syed",
-        email:"syed@gmail.com",
-        password:"12345",
-        age:25
+        firstName:faker.name.firstName(),
+        lastName:faker.name.lastName(),
+        email:faker.internet.email(),
+        password:faker.internet.password(),
+        age:faker.datatype.number({min:18, max:55})
         // age:"25a" //NaN
     })
-
     //storing user instance into our users collection
-    await user.save()
-    console.log("user stored in database now ! ")
-
+    return user.save()
+   /*  console.log("user stored in database now ! ") */
 }
 
-addUser()
+/* for(let i = 0 ; i< 20 ; i++){
+    addUser()
+}
 
+mongoose.connection.close() */
 
+/* const promises = Array(20).fill(null).map(()=>{
+    return addUser() //returns a promise
+})
 
+Promise.all(promises).then(()=>{
+    mongoose.connection.close()
+}) */
 
+// READ DATA from DB ;
+async function getAllUsers(){
+    const users = await UsersCollection.find()
+    console.log(users)
+}
 
+/* getAllUsers() */
 
+async function getSingleUser(){
+ /*    const user = await UsersCollection.findById("635a393963271eba677c56e0") */
+    const user = await UsersCollection.findOne({email: 'Kari15@gmail.com'}) 
+    console.log(user)
+}
 
+/* getSingleUser() */
 
+// UPDATE DATA in DB
+async function updateUser(){
+    const updatedUser = await UsersCollection.findOneAndUpdate({email:'Kari15@gmail.com' }, {age:30} ,{new:true} )
+   /*  const updatedUser = await UsersCollection.findByIdAndUpdate("7583745141f44", {age:32}, {new:true}) */
+    
+    console.log(updatedUser)
+}
 
+/* updateUser() */
 
-
+//DELETE DATA from DB
+async function deleteUsers(){
+   /*  const result = await UsersCollection.deleteMany() */
+    const result = await UsersCollection.deleteOne({_id:"635a393963271eba677c56e0"})
+    console.log(result)
+}
+/* deleteUsers() */
 
 
 
